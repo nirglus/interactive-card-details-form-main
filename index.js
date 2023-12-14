eee9f05d1a9bac196ff40af6f26fb37f460ee27c
nirglus <nirgluskin@gmail.com>
2023-12-14 17:59:05 +0200

    add input listener

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("input", () =>{
     const holderNameInp = document.getElementById("holderName");
     const cardNumInp = document.getElementById("cardNum");
     const expDateMInp = document.getElementById("expDateM");
@@ -9,5 +9,16 @@ document.addEventListener("DOMContentLoaded", () =>{
     const holderNameDisp = document.getElementById("cardHoldName");
     const expDateMDisp = document.getElementById("mmDisplay");
     const expDateYDisp = document.getElementById("yyDisplay");
+
+    holderNameDisp.innerHTML = holderNameInp.value.length == 0 ? 'Jane Appleseed' : holderNameInp.value;
+    cardNumDisp.innerHTML = 
+    cardNumInp.value.length == 0 ? '0000 0000 0000 0000' :
+     cardNumInp.value.replace(/\D+/g, '').replace(/(\d{4})(?=\d)/g, '$1 ');
+
+    cvcDisp.innerHTML = cvcInp.value.length == 0 ? '000' : cvcInp.value;
+    expDateMDisp.innerHTML = expDateMInp.value == 0 ? '00' : expDateMInp.value;
+    expDateYDisp.innerHTML = expDateYInp.value == 0 ? '00' : expDateYInp.value;
+
+
 
 })
