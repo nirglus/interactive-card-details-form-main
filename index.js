3e13d3001792a830a9ea3eca0244937b1cdf0715
nirglus <nirgluskin@gmail.com>
2023-12-14 14:43:50 +0200

    add disp listeners

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ document.addEventListener("DOMContentLoaded", () =>{
     const expDateMInp = document.getElementById("expDateM");
     const expDateYInp = document.getElementById("expDateY");
     const cvcInp = document.getElementById("cvc");
+    const cvcDisp = document.getElementById("cvcDisplay");
+    const cardNumDisp = document.getElementById("cardNumDisplay");
+    const holderNameDisp = document.getElementById("cardHoldName");
+    const expDateMDisp = document.getElementById("mmDisplay");
+    const expDateYDisp = document.getElementById("yyDisplay");
+
 })
