74190db90e93e6e33670b2b825cd1aef5b8d7d66
nirglus <nirgluskin@gmail.com>
2023-12-14 11:59:44 +0200

    add input listeners

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    const holderNameInp = document.getElementById("holderName");
+    const cardNumInp = document.getElementById("cardNum");
+    const expDateMInp = document.getElementById("expDateM");
+    const expDateYInp = document.getElementById("expDateY");
+    const cvcInp = document.getElementById("cvc");
+})
